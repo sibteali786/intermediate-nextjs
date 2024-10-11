1b62e7356f4c286fd0e480ebfdd434cb37c8a93b
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-10-11 15:43:52 +0500

    feat: auth registerUser module

---
+++ actions/auth.ts
@@ -0,0 +1,25 @@
+'use server' // tells react that this file should be run in nodejs environment
+import { cookies } from 'next/headers'
+import { signin, signup } from '@/utils/authTools'
+import { z } from 'zod'
+import { redirect } from 'next/navigation'
+import { COOKIE_NAME } from '@/utils/constants'
+
+const authSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+export const registerUser = async (prevState: any, formData: FormData) => {
+  const data = authSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+  try {
+    const { token } = await signup(data)
+    cookies().set(COOKIE_NAME, token)
+  } catch (e) {
+    console.error(e)
+    return { message: 'Failed to sign you up' }
+  }
+  redirect('/dashboard')
+}
